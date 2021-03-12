a6bee6667066fd91e10ec03b729adc57ced70be4
K0m414 <66835396+K0m414@users.noreply.github.com>
2021-03-12 14:26:33 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,73 +1,90 @@
+'use strict';
 document.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM');
 /*variable*/
+const link = document.getElementById('page-style')
 
-const modeSwitch = document.querySelector('.mode');
-const lightMode = document.querySelector('.fa-sun-o');
-const darkMode = document.querySelector('.fa-moon-o');
-let nightSheet= 'http://test/clavier/css/keyboard-night.css';
-let lightSheet= 'http://test/clavier/css/keyboard-light.css';
-let link = document.getElementById('page-style')
+const nightSheet= 'http://test/clavier/css/keyboard-night.css';
+const lightSheet= 'http://test/clavier/css/keyboard-light.css';
 
-const capsLock = document.querySelector('.capslock');
-const unlock = document.querySelector('.fa-unlock');
-const lock = document.querySelector('.fa-lock');
 const ledOff = document.querySelector('.fa-circle-thin');
 const ledOn = document.querySelector('.fa-dot-circle-o');
-const key = document.querySelectorAll('kbd');
+const lightMode = document.querySelector('.fa-sun-o');
+const darkMode = document.querySelector('.fa-moon-o');
+const unlock = document.querySelector('.fa-unlock');
+const lock = document.querySelector('.fa-lock');
+const write = document.querySelector('textarea');
 
+const modeSwitch = document.querySelector('.mode');
+const capsLock = document.querySelector('.capslock');
 const backspace = document.querySelector('.backspace');
-let write = document.querySelector('textarea');
+const del = document.querySelector('.del');
+const shift1 = document.querySelector('.shift1');
+const shift2 = document.querySelector('.shift2');
+const lettre = document.querySelectorAll('.lettre');
+const key = document.querySelectorAll('kbd');
 let kbd;
 
-let keyEspace = otherElment('.espace', ' ');
-let keyFlech = otherElment('.fleche', '\u2039');
-let keyEnter = otherElment('.enter', '\u000A');
-let keyPoint = otherElment('.point', '\u002E');
-let keyTab = otherElment('.tab', '\u0009');
 
-let keyUn = selectElment('.un');
-let keyDeux = selectElment('.deux');
-let keyTrois = selectElment('.trois');
-let keyQuatre = selectElment('.quatre');
-let keyCinq = selectElment('.cinq');
-let keySIx = selectElment('.six');
-let keySept = selectElment('.sept');
-let keyHuit = selectElment('.huit');
-let keyNeuf = selectElment('.neuf');
-let keyZero = selectElment('.zero');
+lettre.forEach(function(element){
+  selectElment('.a');
+    console.log(element.innerHTML)
+});
+/*lettre*/
+selectElment('.a');
+selectElment('.b');
+selectElment('.c');
+selectElment('.d');
+selectElment('.e');
+selectElment('.f');
+selectElment('.g');
+selectElment('.h');
+selectElment('.i');
+selectElment('.j');
+selectElment('.k');
+selectElment('.l');
+selectElment('.m');
 
-let keyA = selectElment('.a');
-let keyB = selectElment('.b');
-let keyC = selectElment('.c');
-let keyD = selectElment('.d');
-let keyE = selectElment('.e');
-let keyF = selectElment('.f');
-let keyG = selectElment('.g');
-let keyH = selectElment('.h');
-let keyI = selectElment('.i');
-let keyJ = selectElment('.j');
-let keyK = selectElment('.k');
-let keyL = selectElment('.l');
-let keyM = selectElment('.m');
+selectElment('.n');
+selectElment('.o');
+selectElment('.p');
+selectElment('.q');
+selectElment('.r');
+selectElment('.s');
+selectElment('.t');
+selectElment('.u');
+selectElment('.v');
+selectElment('.w');
+selectElment('.x');
+selectElment('.y');
+selectElment('.z');
 
-let keyN = selectElment('.n');
-let keyO = selectElment('.o');
-let keyP = selectElment('.p');
-let keyQ = selectElment('.q');
-let keyR = selectElment('.r');
-let keyS = selectElment('.s');
-let keyT = selectElment('.t');
-let keyU = selectElment('.u');
-let keyV = selectElment('.v');
-let keyW = selectElment('.w');
-let keyX = selectElment('.x');
-let keyY = selectElment('.y');
-let keyZ = selectElment('.z');
+/*autre */
+otherElment('.espace', ' ');
+otherElment('.fleche', '\u2039');
+otherElment('.fleche2', '\u203a ');
+otherElment('.enter', '\u000A');
+otherElment('.point', '\u002E');
+otherElment('.tab', '\u0009');
+otherElment('.point-virgule', '\u037e');
+otherElment('.exclamation', '\u0021');
+otherElment('.interrogation', '\u003F');
+otherElment('.deux-points', '\u003A');
+otherElment('.arobase', '\u0040');
 
-arrayMaj = [];
-keyMaj = document.querySelectorAll('kbd');
-// console.log(keyMaj.forEach())
+/*chiffre */
+selectElment('.un');
+selectElment('.deux');
+selectElment('.trois');
+selectElment('.quatre');
+selectElment('.cinq');
+selectElment('.six');
+selectElment('.sept');
+selectElment('.huit');
+selectElment('.neuf');
+selectElment('.zero');
+
+
 
 /*event listener*/
 
@@ -89,36 +106,44 @@ modeSwitch.addEventListener('click', event => { //clique sur la touche mode
 
     // Supprimer un caractère
     backspace.addEventListener('click', () => {
-      let keyBack = write.value.slice(0, write.value.length - 1)
+      let keyBack = write.value.slice(0, write.value.length - 1);
       // console.log(keyBack)
       write.value = keyBack
   })
 
+    // supprimer tous les caractere
+  del.addEventListener('click', () => {
+    let delValue = write.value
+    // console.log(delValue)
+    write.value = ''
+  })
 
 /*pour activer ou désactiver caps lock*/
   capsLock.addEventListener('click', event => {
 
-    // console.log('clique maj');
     //verrou
-    unlock.classList.toggle('hidden') // cache l'icône en changeant de classe
-    // unlock.classList.add('fgd')
-    lock.classList.toggle('visible') //affiche l'icône en changeant de classe
+    unlock.classList.toggle('hidden'); // cache l'icône en changeant de classe
+    lock.classList.toggle('visible'); //affiche l'icône en changeant de classe
+    
     //led
-    ledOff.classList.toggle('hidden') // cache l'icône en changeant de classe
-    ledOn.classList.toggle('visible') //affiche l'icône en changeant de classe
+    ledOff.classList.toggle('hidden'); // cache l'icône en changeant de classe
+    ledOn.classList.toggle('visible'); //affiche l'icône en changeant de classe
 
     /*Mettre en maj*/
-     keyMaj.forEach(element => element.classList.toggle('maj'))
-    // keyMaj.forEach(element => console.log(element))
-    
-  });
+    key.forEach(function(element){
+      element.classList.toggle('maj')
+        // console.log(element.innerText)
+    });
 
+  });
+  
 /*fonction */
 
 /*en maj*/
 function toUpper(text){
   return text.toUpperCase()
 }
+/*change de feuille de style */
   function changerDeStyle(sheet){
     link.setAttribute('href', sheet);
     // console.log(link)    
@@ -126,29 +151,20 @@ function toUpper(text){
 
 // function qui permet de choisir la touche a écrire
   function selectElment(className){
-
-    kbd = document.querySelector(className);
-    // innerText applies text-transform and white-space rules
-    let touchName = kbd.innerText;
-
-    kbd.addEventListener('click', event => {
-      if (write.value === ''){
-        write.value = touchName
-      }
-      else{
-        write.value +=touchName
-      }
-    
-    // console.log(touchName)
+    kbd = document.querySelector(className); // selectionne la class 
+    let toucheName = kbd.innerText; //selection l'element dans la touche = innerText
+    // mettre en maj => toucheName = toUpper(kbd.innerText);
+  
+    kbd.addEventListener('click', event => { // event listener au click  
+        write.value +=toucheName //ecrit l'element selectionné sur le texterea
+    console.log(toucheName)
     })
   }
 
-
-  function otherElment(className, elementValue){
-
-    kbd = document.querySelector(className);
+  function otherElment(className, elementValue){// fonction avec deux valeurs le nom de la classe et la valeur
+    kbd = document.querySelector(className);// selectionne la class 
     kbd.addEventListener('click', event => {
-        write.value += elementValue
+        write.value += elementValue //ecrit la valeur selectionné sur le texterea
     // console.log(elementValue)
     })
   }
